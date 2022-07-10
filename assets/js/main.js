22fff49128d6f2cc88d52b1f7132d4d830afbd0d
unknown <you@example.com>
2022-07-10 20:43:28 +0300

    dark and light theme. rework mistakes and bad code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,32 @@ $(document).ready(function() {
     $('body').toggleClass('lock');
   })
 });
+
+
+// Светлая и темная темы
+
+// Функция для установки заданной темы/цветовой схемы
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+}
+
+// Функция для переключения между темной и светлой темой
+function toggleTheme() {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+      setTheme('theme-light');
+  } else {
+      setTheme('theme-dark');
+  }
+}
+
+// Функция дя немедленной загрузки
+(function () {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+      setTheme('theme-dark');
+      document.getElementById('dark').checked = false;
+  } else {
+      setTheme('theme-light');
+    document.getElementById('dark').checked = true;
+  }
+})();
